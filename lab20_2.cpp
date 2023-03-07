ecc4f8d158e293d46c43429dfebab1e095a4f306
ShinRunner1st <shinrunner1st@gmail.com>
2023-03-08 00:50:30 +0700

    f

--- lab20_2.cpp
+++ lab20_2.cpp
@@ -5,8 +5,15 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect *r1, Rect *r2)
+{
+    double x_d = min(r1->x+r1->w, r2->x+r2->w) - max(r1->x, r2->x);
+    double y_d = (min(r1->y, r2->y) - max(r1->y-r1->h, r2->y-r2->h));
+    double aI = 0;
+    if( x_d > 0 && y_d > 0 )
+    {
+        aI = x_d * y_d;
+    }
+      
+    return (aI);
 }
